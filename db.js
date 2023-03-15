4c64ea479bf9be6b98d316f7c80e97134658bee2
PaAdRaAg <pramos12@ucol.mx>
2023-03-14 23:16:32 -0600

    conectar a bd

---
+++ db.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+
+const UserModel = require('./models/users');
+
+const sequelize = new Sequelize('mydatabase', 'root', '', {
+    host: '127.0.0.1',
+    dialect: 'mysql'
+});
+
+const User = UserModel(sequelize, Sequelize);
+
+sequelize.sync({ force: false })
+    .then(() => {
+        console.log('Database & tables synced!');
+    });
+    
+module.exports = { 
+    User
+};

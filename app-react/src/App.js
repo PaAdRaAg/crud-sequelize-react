5eb8a7b6213946308910345f326614415f30f896
PaAdRaAg <pramos12@ucol.mx>
2023-03-16 11:41:29 -0600

    ag modulos y fetch al serv

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment, useState, useEffect} from 'react';
+import Navbar from './components/Navbar';
+import UsersList from './components/UsersList';
 
 function App() {
+
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    const getUsers = () => {
+      fetch('http://localhost:3001/api/users')
+      .then(res => res.json())
+      .then(res => setUsers(res))
+    }
+    getUsers()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Navbar brand='CRUD Sequelize,React and Node App'/>
+      <div className="container">
+        <div className="row">
+          <div className="col-7">
+            <h2 style={{textAlign: 'center'}}>Lista de Usuarios</h2>
+            <UsersList users={users}/>
+          </div>
+          <div className="col-5">
+            <h2 style={{textAlign: 'center'}}>Agragar Usuario</h2>
+
+          </div>
+        </div>
+      </div>
+    </Fragment>
+
   );
 }
 

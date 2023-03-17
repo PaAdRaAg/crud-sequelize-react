fb8ee7801adf5031c969461e3e29c1f4de4f80ed
PaAdRaAg <pramos12@ucol.mx>
2023-03-16 18:46:12 -0600

    ajustar a sqlserver

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const UserModel = require('./models/users');
 
-const sequelize = new Sequelize('mydatabase', 'root', '', {
+const sequelize = new Sequelize('mydatabase', 'root', 'Adrian_090702', {
     host: '127.0.0.1',
     dialect: 'mysql'
 });
